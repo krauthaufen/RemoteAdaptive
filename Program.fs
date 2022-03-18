60510d2692c58e19cbe9974bf37934db875ebb0c
krauthaufen <krauthaufen@awx.at>
2022-03-18 21:01:36 +0100

    AList.observe

--- Program.fs
+++ Program.fs
@@ -12,49 +12,21 @@ let input = clist ["a"; "b"]
 let dependentList = 
     input
     |> AList.map (fun el -> sprintf "%s:%d" el el.Length)
+    |> AList.takeA (cval 10)
     |> AList.pairwise
 
 
-
-// here we setup a thread that blocks until 'changesPending' is set to true
-// by the use of a standard-monitor
-let trigger = obj()
-let mutable changesPending = true
-
-// a reader is a stateful "listener" on the list that can be queried for
-// pending changes. Much like IEnumerator<'a> for seq<'a>
-let reader = dependentList.GetReader()
-
-// whenever there are changes to fetch trigger our thread
 let subscription = 
-    reader.AddMarkingCallback (fun () ->
-        lock trigger (fun () ->
-            changesPending <- true
-            Monitor.PulseAll trigger
-        )
+    dependentList |> AList.observe (fun state changes ->
+        for index, operation in changes do
+            match operation with
+            | Set value -> 
+                printfn "   set(%A, %A)" index value
+            | Remove ->
+                printfn "   rem(%A)" index
+
+        printfn "   result: %0A" (IndexList.toList state)
     )
-
-startThread <| fun () ->
-    while true do
-        lock trigger (fun () ->
-            while not changesPending do
-                Monitor.Wait trigger |> ignore
-            changesPending <- false
-        )
-        
-        let changes = reader.GetChanges AdaptiveToken.Top
-        if not (IndexListDelta.isEmpty changes) then
-            let state = reader.State
-
-            for index, operation in changes do
-                match operation with
-                | Set value -> 
-                    printfn "   set(%A, %A)" index value
-                | Remove ->
-                    printfn "   rem(%A)" index
-
-            printfn "   result: %0A" (IndexList.toList state)
-
 
 // here's some code to update the list via the command-line
 //  `+<name>` inserts `name` at a random position in the list
--- Utilities.fs
+++ Utilities.fs
@@ -41,3 +41,48 @@ let mainLoop(input : clist<string>) =
             exit 0
         else
             err "bad command: %A" line
+
+
+module AList =
+    let observe (action : IndexList<'a> -> IndexListDelta<'a> -> unit) (list : alist<'a>) =
+
+        // here we setup a thread that blocks until 'changesPending' is set to true
+        // by the use of a standard-monitor
+        let trigger = obj()
+        let mutable changesPending = true
+
+        // a reader is a stateful "listener" on the list that can be queried for
+        // pending changes. Much like IEnumerator<'a> for seq<'a>
+        let reader = list.GetReader()
+
+        // whenever there are changes to fetch trigger our thread
+        let subscription = 
+            reader.AddMarkingCallback (fun () ->
+                lock trigger (fun () ->
+                    changesPending <- true
+                    Monitor.PulseAll trigger
+                )
+            )
+
+        let mutable running = true
+
+        startThread <| fun () ->
+            while running do
+                lock trigger (fun () ->
+                    while not changesPending do
+                        Monitor.Wait trigger |> ignore
+                    changesPending <- false
+                )
+                if running then
+                    let changes = reader.GetChanges AdaptiveToken.Top
+                    // due to the way adaptive works internally there might be "dummy"-changes in certain scenarios
+                    if not (IndexListDelta.isEmpty changes) then
+                        let state = reader.State
+                        action state changes
+                    
+        { new IDisposable with
+            member x.Dispose() =
+                subscription.Dispose()
+                running <- false
+                lock trigger (fun () -> changesPending <- true; Monitor.PulseAll trigger)
+        }
